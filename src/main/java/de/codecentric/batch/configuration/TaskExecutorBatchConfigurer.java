b207d35722622c4aa1e4eac60eeae01f5851663f
Dennis Schulte <mail@dennis-schulte.de>
2014-10-10 12:25:15 +0200

    Add new configuration options

--- src/main/java/de/codecentric/batch/configuration/TaskExecutorBatchConfigurer.java
+++ src/main/java/de/codecentric/batch/configuration/TaskExecutorBatchConfigurer.java
@@ -46,14 +46,24 @@ import org.springframework.transaction.PlatformTransactionManager;
  * {@link org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer}, it only
  * introduces a {@link org.springframework.core.task.TaskExecutor} used in the {@link org.springframework.batch.core.launch.support.SimpleJobLauncher}
  * for starting jobs
- * asynchronously. Its thread pool is configured to five threads by default, which can be changed
- * by setting the property batch.core.pool.size/batch.max.pool.size to a different number.
+ * asynchronously. Its core thread pool is configured to five threads by default, which can be changed
+ * by setting the property batch.core.pool.size to a different number. 
+ * 
+ * Please note the following rules of the ThreadPoolExecutor:
+ * If the number of threads is less than the corePoolSize, the executor creates a new thread to run a new task.
+ * If the number of threads is equal (or greater than) the corePoolSize, the executor puts the task into the queue.
+ * If the queue is full and the number of threads is less than the maxPoolSize, the executor creates a new thread to run a new task.
+ * If the queue is full and the number of threads is greater than or equal to maxPoolSize, reject the task.
+ * 
+ * So with the default configuration there will be only 5 jobs/threads at the same time.
+ * 
  * The {@link org.springframework.core.task.TaskExecutor} may also be used in job configurations
  * for multi-threaded job execution. In XML you can use it by name, which is taskExecutor. In JavaConfig,
  * you can either autowire {@link org.springframework.core.task.TaskExecutor} or, if you want to know
  * where it's configured, this class.
  * 
  * @author Tobias Flohre
+ * @author Dennis Schulte
  * 
  */
 @ConditionalOnMissingBean(BatchConfigurer.class)
@@ -74,8 +84,9 @@ public class TaskExecutorBatchConfigurer implements BatchConfigurer {
 	@Bean
 	public TaskExecutor taskExecutor() {
 		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-		taskExecutor.setCorePoolSize(Integer.parseInt(env.getProperty("batch.core.pool.size", "5")));
-		taskExecutor.setMaxPoolSize(Integer.parseInt(env.getProperty("batch.max.pool.size", "5")));
+		taskExecutor.setCorePoolSize(env.getProperty("batch.core.pool.size", Integer.class, 5));
+		taskExecutor.setQueueCapacity(env.getProperty("batch.queue.capacity", Integer.class, Integer.MAX_VALUE));
+		taskExecutor.setMaxPoolSize(env.getProperty("batch.max.pool.size", Integer.class, Integer.MAX_VALUE));
 		taskExecutor.afterPropertiesSet();
 		return taskExecutor;
 	}
