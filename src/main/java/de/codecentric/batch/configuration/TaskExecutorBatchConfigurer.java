6cf25a14ac7a59e42e990a2e435ab9d96edf6b09
Dennis Schulte <mail@dennis-schulte.de>
2014-11-20 19:14:28 +0100

    Create separate TaskExecutorConfiguration

--- src/main/java/de/codecentric/batch/configuration/TaskExecutorBatchConfigurer.java
+++ src/main/java/de/codecentric/batch/configuration/TaskExecutorBatchConfigurer.java
@@ -33,34 +33,17 @@ import org.springframework.batch.core.repository.support.MapJobRepositoryFactory
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * This batch infrastructure configuration is quite similar to the
  * {@link org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer}, it only
- * introduces a {@link org.springframework.core.task.TaskExecutor} used in the {@link org.springframework.batch.core.launch.support.SimpleJobLauncher}
- * for starting jobs
- * asynchronously. Its core thread pool is configured to five threads by default, which can be changed
- * by setting the property batch.core.pool.size to a different number. 
- * 
- * Please note the following rules of the ThreadPoolExecutor:
- * If the number of threads is less than the corePoolSize, the executor creates a new thread to run a new task.
- * If the number of threads is equal (or greater than) the corePoolSize, the executor puts the task into the queue.
- * If the queue is full and the number of threads is less than the maxPoolSize, the executor creates a new thread to run a new task.
- * If the queue is full and the number of threads is greater than or equal to maxPoolSize, reject the task.
- * 
- * So with the default configuration there will be only 5 jobs/threads at the same time.
- * 
- * The {@link org.springframework.core.task.TaskExecutor} may also be used in job configurations
- * for multi-threaded job execution. In XML you can use it by name, which is taskExecutor. In JavaConfig,
- * you can either autowire {@link org.springframework.core.task.TaskExecutor} or, if you want to know
- * where it's configured, this class.
+ * references a {@link org.springframework.core.task.TaskExecutor} used in the {@link org.springframework.batch.core.launch.support.SimpleJobLauncher}
+ * for starting jobs asynchronously. 
  * 
  * @author Tobias Flohre
  * @author Dennis Schulte
@@ -74,22 +57,17 @@ public class TaskExecutorBatchConfigurer implements BatchConfigurer {
 
 	@Autowired
 	private Environment env;
-
+	
+	// Created by TaskExecutorConfiguration if it is used. If an alternative TaskExecutor is configured, 
+	// it will be injected here.
+	@Autowired
+	private TaskExecutor taskExecutor;
+	
 	private DataSource dataSource;
 	private PlatformTransactionManager transactionManager;
 	private JobRepository jobRepository;
 	private JobLauncher jobLauncher;
 	private JobExplorer jobExplorer;
-
-	@Bean
-	public TaskExecutor taskExecutor() {
-		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-		taskExecutor.setCorePoolSize(env.getProperty("batch.core.pool.size", Integer.class, 5));
-		taskExecutor.setQueueCapacity(env.getProperty("batch.queue.capacity", Integer.class, Integer.MAX_VALUE));
-		taskExecutor.setMaxPoolSize(env.getProperty("batch.max.pool.size", Integer.class, Integer.MAX_VALUE));
-		taskExecutor.afterPropertiesSet();
-		return taskExecutor;
-	}
 
 	@Autowired
 	public void setDataSource(DataSource dataSource) {
@@ -120,7 +98,7 @@ public class TaskExecutorBatchConfigurer implements BatchConfigurer {
 	private JobLauncher createJobLauncher() throws Exception {
 		SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
 		jobLauncher.setJobRepository(jobRepository);
-		jobLauncher.setTaskExecutor(taskExecutor());
+		jobLauncher.setTaskExecutor(taskExecutor);
 		jobLauncher.afterPropertiesSet();
 		return jobLauncher;
 	}
---
+++ src/main/java/de/codecentric/batch/configuration/TaskExecutorConfiguration.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.codecentric.batch.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * This is the default configuration for a {@link org.springframework.core.task.TaskExecutor} used in the {@link org.springframework.batch.core.launch.support.SimpleJobLauncher}
+ * for starting jobs asynchronously. Its core thread pool is configured to five threads by default, which can be changed
+ * by setting the property batch.core.pool.size to a different number. 
+ * 
+ * Please note the following rules of the ThreadPoolExecutor:
+ * If the number of threads is less than the corePoolSize, the executor creates a new thread to run a new task.
+ * If the number of threads is equal (or greater than) the corePoolSize, the executor puts the task into the queue.
+ * If the queue is full and the number of threads is less than the maxPoolSize, the executor creates a new thread to run a new task.
+ * If the queue is full and the number of threads is greater than or equal to maxPoolSize, reject the task.
+ * 
+ * So with the default configuration there will be only 5 jobs/threads at the same time.
+ * 
+ * The {@link org.springframework.core.task.TaskExecutor} may also be used in job configurations
+ * for multi-threaded job execution. In XML you can use it by name, which is taskExecutor. In JavaConfig,
+ * you can either autowire {@link org.springframework.core.task.TaskExecutor} or, if you want to know
+ * where it's configured, this class.
+ * 
+ * @author Dennis Schulte
+ * 
+ */
+@Configuration
+@ConditionalOnMissingBean(TaskExecutor.class)
+public class TaskExecutorConfiguration {
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public TaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(env.getProperty("batch.core.pool.size", Integer.class, 5));
+		taskExecutor.setQueueCapacity(env.getProperty("batch.queue.capacity", Integer.class, Integer.MAX_VALUE));
+		taskExecutor.setMaxPoolSize(env.getProperty("batch.max.pool.size", Integer.class, Integer.MAX_VALUE));
+		taskExecutor.afterPropertiesSet();
+		return taskExecutor;
+	}
+
+}
