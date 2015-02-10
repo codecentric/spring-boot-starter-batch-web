0798cc83ae185376d89817168006bbda5c0f4cf2
Dennis Schulte <mail@dennis-schulte.de>
2015-02-10 11:32:58 +0100

    React on org.springframework.boot.context.event.ApplicationPreparedEvent for Log Init

--- src/main/java/de/codecentric/batch/listener/LoggingApplicationListener.java
+++ src/main/java/de/codecentric/batch/listener/LoggingApplicationListener.java
@@ -1,7 +1,7 @@
 package de.codecentric.batch.listener;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
 
@@ -9,22 +9,23 @@ import org.springframework.core.Ordered;
  * This ApplicationListener makes the batch.joblog.path available before the LoggingSystem is started.
  * @author Johannes Stelzer
  */
-public class LoggingApplicationListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, Ordered {
+public class LoggingApplicationListener implements ApplicationListener<ApplicationPreparedEvent>, Ordered {
 
-	@Override
-	public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
-		String jobLogPath = event.getEnvironment().getProperty("batch.joblog.path");
-		if (!StringUtils.isEmpty(jobLogPath)) {
-		        if (!jobLogPath.endsWith("/")) {
-			        jobLogPath = jobLogPath + "/";
-		        }
-			System.setProperty("JOB_LOG_PATH", jobLogPath);
-		}
-	}
+    @Override
+    public void onApplicationEvent(ApplicationPreparedEvent event) {
+            String jobLogPath = event.getApplicationContext().getEnvironment().getProperty("batch.joblog.path");
+            if (!StringUtils.isEmpty(jobLogPath)) {
+                    if (!jobLogPath.endsWith("/")) {
+                            jobLogPath = jobLogPath + "/";
+                    }
+                    System.setProperty("JOB_LOG_PATH", jobLogPath);
+            }
+    }
 
-	@Override
-	public int getOrder() {
-		return Ordered.HIGHEST_PRECEDENCE + 10;
-	}
+    @Override
+    public int getOrder() {
+            return Ordered.HIGHEST_PRECEDENCE + 10;
+    }
 
 }
+
